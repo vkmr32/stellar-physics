14908def2a64071c6bcf1e85678942a1cafba368
Vishnu Jay Kumar <vkmr327@gmail.com>
2026-01-28 22:21:03 -0500

    checking

--- star_sim.cpp
+++ star_sim.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <vector>
 
 GLFWwindow* StartGLFW();
 
